fcdc3ccd9cccdd4e3ebd037d1f1b0f954de3d198
Vinicius <viniciuscarvalho68993@gmail.com>
2025-06-06 14:36:07 -0300

    Create Aprendendo.py

---
+++ Aprendendo.py
@@ -0,0 +1,26 @@
+def main():
+    print("Olá, mundo!")
+    Nome= 'vinicius'
+    idade= '22'
+    peso='53'
+   
+nome=input('qual é o seu nome?')
+print(' Seja bem vindo prazer em te conhecer')
+idade=input(' qual é a sua idade?')
+print('Otimo')
+peso=input(' qual é o seu peso?')
+prentende=input('Qual a data que vai começar a aprender python?')
+print('Isso ai, o primeiro passo é o maior de todos') 
+sonhos=input('Porque esta tentando aprender isso?')
+print('Continue!')
+onde=input('Onde você quer chegar?')
+print('Acredito em você!!')
+print('confira os seus dados baixo')
+print(nome, idade, peso)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
