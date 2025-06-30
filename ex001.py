75a3c46d1cc2fa9d6fdeff18584f040f7800c2c6
Vinicius <viniciuscarvalho68993@gmail.com>
2025-06-30 19:00:38 -0300

    testando

--- ex001.py
+++ ex001.py
@@ -1,7 +1,8 @@
 from Aprendendo import idade
 
 nome=input('qual é o seu nome?')
-print(' É um prazer em te conhecer {}!')
+print(' É um prazer em te conhecer, {}!.format(nome)vinicius'
+      '')
 idade=input('qual  o seu idade?')
 peso=input('qual  o seu peso?')
 print(nome,idade,peso)
